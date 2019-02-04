433f9ec3ce0ab55fe18f26039469722e1ca1f6b6
Raiyan Shaheen <r.12345@hotmail.co.uk>
2019-02-04 16:16:28 +0000

    added tests to spec test to all fileds corresponding to the functions

--- lib/services/CurrentPopularMovies.rb
+++ lib/services/CurrentPopularMovies.rb
@@ -31,6 +31,135 @@ class CurrentPopularMovies
     retrieve_result['num_results']
   end
 
+  def retrieve_result_array
+    retrieve_result['results']
+  end
+
+  def retrieve_all_display_title
+
+    display_title = []
+    retrieve_result_array.each do |title|
+      display_title << title['display_title']
+    end
+    display_title
+  end
+
+  def retrieve_all_mpaa_rating
+
+    mpaa_rating = []
+    retrieve_result_array.each do |result|
+      mpaa_rating << result['mpaa_rating']
+    end
+    mpaa_rating
+  end
+
+  def retrieve_all_critics_pick
+
+    critics_pick = []
+    retrieve_result_array.each do |result|
+      critics_pick << result['critics_pick']
+    end
+    critics_pick
+  end
+
+  def retrieve_all_byline
+
+    byline = []
+    retrieve_result_array.each do |result|
+      byline << result['byline']
+    end
+    byline
+  end
+
+  def retrieve_all_headline
+
+    headline = []
+    retrieve_result_array.each do |result|
+      headline << result['headline']
+    end
+    headline
+  end
+
+  def retrieve_all_summary_short
+
+    summary_short = []
+    retrieve_result_array.each do |result|
+      summary_short << result['summary_short']
+    end
+    summary_short
+  end
+
+  def retrieve_all_publication_date
+
+    publication_date = []
+    retrieve_result_array.each do |result|
+      publication_date << result['publication_date']
+    end
+    publication_date
+  end
+
+  def retrieve_all_opening_date
+
+    opening_date = []
+    retrieve_result_array.each do |result|
+      opening_date << result['opening_date']
+    end
+    opening_date
+  end
+
+  def retrieve_all_date_updated
+
+    date_updated = []
+    retrieve_result_array.each do |result|
+      date_updated << result['date_updated']
+    end
+    date_updated
+  end
+
+  def retrieve_all_link
+
+    link = []
+    retrieve_result_array.each do |result|
+      link << result['link']
+    end
+    link
+  end
+
+  def retrieve_all_link_type
+
+    link_type = []
+    retrieve_result_array.each do |result|
+      link_type << result['link']['type']
+    end
+    link_type
+  end
+
+  def retrieve_all_link_url
+
+    link_url = []
+    retrieve_result_array.each do |result|
+      link_url << result['link']['url']
+    end
+    link_url
+  end
+
+  def retrieve_all_link_suggested_link_text
+
+    suggested_link_text = []
+    retrieve_result_array.each do |result|
+      suggested_link_text << result['link']['suggested_link_text']
+    end
+    suggested_link_text
+  end
+
+  def retrieve_all_multimedia
+
+    multimedia = []
+    retrieve_result_array.each do |result|
+      multimedia << result['multimedia']
+    end
+    multimedia
+  end
 
 end
 
--- spec/CurrentPopular_spec.rb
+++ spec/CurrentPopular_spec.rb
@@ -9,26 +9,148 @@ describe NYTimes do
       # p @NYTio.retrieve_current(ENV['API_KEY'],7)
     end
 
-    it "should have api respond with a hash" do
-      expect(@NYTio.retrieve_data).to be_kind_of(Hash)
+    it 'should retrieve the results in as a hash' do 
+      expect(@NYTimes.retrieve_result).to be_kind_of(Hash)
     end
 
-    it "should return an OK status be string" do 
-      expect(@NYTio.retrieve_status).to be_kind_of(String)
+    it 'should retrieve the status as a string' do 
+      expect(@NYTimes.retrieve_status).to be_kind_of(String)
     end
 
-    it "should return an OK message" do 
-      expect(@NYTio.retrieve_status).to match('OK')
+    it 'should retrieve the copyright as a string' do 
+      expect(@NYTimes.retrieve_copyright).to be_kind_of(String)
     end
 
-    it "should return number of results as an integer" do
-      expect(@NYTio.retrieve_results).to be_kind_of(Integer)
-      p 
+    it 'should retrieve the has more as a boolean' do 
+      expect(@NYTimes.retrieve_has_more).to eq(true) | eq(false)
     end
 
-    it "should return an array" do 
-      p @NYTio.retrieve_results_url
-      # expect(@NYTio.retrieve_results_url).to be_kind_of(Array)
+    it 'should retrieve the number results as an integer' do 
+      expect(@NYTimes.retrieve_num_results).to be_kind_of(Integer)
+    end
+
+    it 'should retrieve the results array as an array' do 
+      expect(@NYTimes.retrieve_result_array).to be_kind_of(Array)
+    end
+
+    it 'should retrieve all display title from results array as a string' do 
+      @NYTimes.retrieve_all_display_title.each do |display_title| 
+        expect(display_title).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all mpaa_rating from results array as a string' do 
+      @NYTimes.retrieve_all_mpaa_rating.each do |mpaa_rating| 
+        expect(mpaa_rating).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all critics_pick from results array as integer' do 
+      @NYTimes.retrieve_all_critics_pick.each do |critics_pick| 
+        expect(critics_pick).to be_kind_of(Integer)
+      end
+    end
+
+    it 'should retrieve all byline from results array as a string' do 
+      @NYTimes.retrieve_all_byline.each do |byline|
+        expect(byline).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all headline from results array as a string' do 
+      @NYTimes.retrieve_all_headline.each do |headline|
+        expect(headline).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all summary short from results array as a string' do 
+      @NYTimes.retrieve_all_summary_short.each do |summary_short|
+        expect(summary_short).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all publication date from results array as a string' do 
+      @NYTimes.retrieve_all_publication_date.each do |publication_date|
+        expect(publication_date).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all opening_date from results array as a string' do 
+      @NYTimes.retrieve_all_opening_date.each do |opening_date|
+        expect(opening_date).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all date_updated from results array as a string' do 
+      @NYTimes.retrieve_all_date_updated.each do |date_updated|
+        expect(date_updated).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all link from results array as a hash' do 
+      @NYTimes.retrieve_all_link.each do |link|
+        expect(link).to be_kind_of(Hash)
+      end
+    end
+    
+    it 'should retrieve all link types from results array as a string' do 
+      @NYTimes.retrieve_all_link_type.each do |link_type|
+        expect(link_type).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all link urls from results array as a string' do 
+      @NYTimes.retrieve_all_link_url.each do |url|
+        expect(url).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all link suggested link text from results array as a string' do 
+      @NYTimes.retrieve_all_link_suggested_link_text.each do |suggested_link_text|
+        expect(suggested_link_text).to be_kind_of(String)
+      end
+    end
+
+    it 'should retrieve all multimedia from results array as null' do 
+      @NYTimes.retrieve_all_multimedia.each do |multimedia|
+        expect(multimedia).to be(nil)
+      end
+    end
+
+    it 'should include the title name in all display titles from results array' do 
+      @NYTimes.retrieve_all_display_title.each do |display_title| 
+        expect(display_title).to include("Pokemon")
+      end
+    end
+
+    it 'should retrieve all mpaa_rating from results array with the length of 1' do 
+      @NYTimes.retrieve_all_mpaa_rating.each do |mpaa_rating| 
+        expect(mpaa_rating.length).to eq 1
+      end
+    end
+
+    it 'should retrieve all publication date from results array with a length of 10' do 
+      @NYTimes.retrieve_all_publication_date.each do |publication_date|
+        expect(publication_date.length).to eq 10
+      end
+    end
+
+    it 'should retrieve all opening_date from results array with a length of 10' do 
+      @NYTimes.retrieve_all_opening_date.each do |opening_date|
+        expect(opening_date.length).to eq 10
+      end
+    end
+
+    it 'should retrieve all date_updated from results array with a length of 10' do 
+      @NYTimes.retrieve_all_date_updated.each do |date_updated|
+        expect(date_updated.length).to eq 19
+      end
+    end
+
+    it 'should include http:// in  all link urls from results array' do 
+      @NYTimes.retrieve_all_link_url.each do |url|
+        expect(url).to include('http://')
+      end
     end
 
   end
