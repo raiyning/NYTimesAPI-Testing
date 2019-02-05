af7c764f5ecd5f8a7a654dbeaeb0e92673ac6cf2
Raiyan Shaheen <r.12345@hotmail.co.uk>
2019-02-05 08:47:49 +0000

    changed all refernces on readme to only this code and changed the description language

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # NYTimesAPI-Testing
 ### Description
 
-This is an attempt to create a Test which on the https://developer.nytimes.com/get-started API service with documentation on the website we tried to test all the fields.  The program will test a different parameters of the API file recieved from NYTimes from the website. . 
+I have created a service object model (SOM) which takes data from https://developer.nytimes.com/get-started API service. Using documentation on the website we tried to test all the fields specifically for movie reviews API and have created unit tests that test the individual datasets and their data types using rspec.
+
 ### Languages used
 * Ruby 
 
@@ -15,13 +16,15 @@ This is an attempt to create a Test which on the https://developer.nytimes.com/g
 1. To download a copy click on 'clone or download' and you can download it as a zip file or git clone it.
 2. Unzip the file
 3. Open up the folder
-4. make sure you're in the directory of API-RandomPGenerator
+4. make sure you're in the directory of NYTimes-Testing
 
 ### How to test
 1. Bundle install to download dependicies
-2. open https://openweathermap.org/current and create your own API key to use
+2. open https://developer.nytimes.com/get-started and create your own API key to use
 3. Create a empty .env file and type out
-> API_KEY= {insert_your_api_key_without_brackets}
+```
+API_KEY={insert_your_api_key_without_brackets}
+```
 4. Save file 
 5. Within terminal type rspec 
 6. Tests should then display in the terminal, whether they're correct or incorrect with associated contexts
@@ -31,13 +34,13 @@ This is an attempt to create a Test which on the https://developer.nytimes.com/g
 > lib -> generator -> services
 2. Copy the city_list.json
 3. Require relative to the class file 
-4. To create the object to use, instanciate the Parse_json_cities
+4. To create the object to use, instanciate the RandomMovies object
 ```ruby
 test = RandomMoviesGenerator.new.get_movie_by_name
 ```
 5. This will return a random movie string with 2500 different movies
 
-6. This will return with cityID number usable with weather API
+6. This will return with a movie string usable with NYtimes API
 >'pokemon'
 
 ### Challenges 
@@ -45,7 +48,7 @@ test = RandomMoviesGenerator.new.get_movie_by_name
 2. Finding out the API keys from NYTimes
 3. More tests rather than datastypes
 
-
 ### Learning points
 1. Structure classes using SOM principles
-2. try to find out difficult API key
+2. Trying to find out a difficult API key
+3. Looping for larger datasets 
--- lib/services/CurrentPopularMovies.rb
+++ lib/services/CurrentPopularMovies.rb
@@ -33,13 +33,11 @@ class CurrentPopularMovies
     retrieve_result['num_results']
   end
 
-  # creating loops to iterate through every hash so code is kept DRY 
   def retrieve_result_array
     retrieve_result['results']
   end
 
   def retrieve_all_display_title
-
     display_title = []
     retrieve_result_array.each do |title|
       display_title << title['display_title']
@@ -48,7 +46,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_mpaa_rating
-
     mpaa_rating = []
     retrieve_result_array.each do |result|
       mpaa_rating << result['mpaa_rating']
@@ -57,7 +54,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_critics_pick
-
     critics_pick = []
     retrieve_result_array.each do |result|
       critics_pick << result['critics_pick']
@@ -66,7 +62,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_byline
-
     byline = []
     retrieve_result_array.each do |result|
       byline << result['byline']
@@ -75,7 +70,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_headline
-
     headline = []
     retrieve_result_array.each do |result|
       headline << result['headline']
@@ -84,7 +78,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_summary_short
-
     summary_short = []
     retrieve_result_array.each do |result|
       summary_short << result['summary_short']
@@ -93,7 +86,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_publication_date
-
     publication_date = []
     retrieve_result_array.each do |result|
       publication_date << result['publication_date']
@@ -102,7 +94,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_opening_date
-
     opening_date = []
     retrieve_result_array.each do |result|
       opening_date << result['opening_date']
@@ -111,7 +102,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_date_updated
-
     date_updated = []
     retrieve_result_array.each do |result|
       date_updated << result['date_updated']
@@ -120,7 +110,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_link
-
     link = []
     retrieve_result_array.each do |result|
       link << result['link']
@@ -129,7 +118,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_link_type
-
     link_type = []
     retrieve_result_array.each do |result|
       link_type << result['link']['type']
@@ -138,7 +126,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_link_url
-
     link_url = []
     retrieve_result_array.each do |result|
       link_url << result['link']['url']
@@ -147,7 +134,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_link_suggested_link_text
-
     suggested_link_text = []
     retrieve_result_array.each do |result|
       suggested_link_text << result['link']['suggested_link_text']
@@ -156,7 +142,6 @@ class CurrentPopularMovies
   end
 
   def retrieve_all_multimedia
-
     multimedia = []
     retrieve_result_array.each do |result|
       multimedia << result['multimedia']
